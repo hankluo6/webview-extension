5b9701f547fc0783148012e7dc4862a6758a4e4d
hank <hankluo6@gmail.com>
2023-06-14 12:51:40 +0800

    Initial commit

---
+++ media/panel.js
@@ -0,0 +1,175 @@
+
+var painterElement = /** @type {HTMLCanvasElement} */ (document.getElementById("painter"));
+var context = painterElement?.getContext('2d');
+var paint = false;
+var lastPos = {x: 0, y: 0};
+var mode;
+const drawingHistory = /** @type {(string | undefined)[]} */ ([]);
+var historyPointer = -1;
+const maxHistoryLength = 100;
+
+// /* initialization */
+// drawingHistory.push(context?.canvas.toDataURL());
+
+saveCanvasState();
+
+function mousedown(/** @type {MouseEvent} */ e) {
+	setPosition(e)
+}
+
+function mouseup(/** @type {MouseEvent} */ e) {
+	setPosition(e)
+	saveCanvasState()
+}
+
+function setPosition(/** @type {MouseEvent} */ e) {
+	lastPos.x = e.pageX;
+	lastPos.y = e.pageY;
+}
+
+// painterElement?.addEventListener('mousedown', setPosition)
+
+painterElement?.addEventListener('mousedown', mousedown)
+
+painterElement?.addEventListener('mouseup', mouseup)
+
+painterElement?.addEventListener('mousemove', function(e) {
+	var color = /** @type {HTMLInputElement} */ (document.getElementById('hex'))?.value;
+	if (e.buttons != 1)
+		return;
+	context?.beginPath();
+	context?.moveTo(lastPos.x - this.offsetLeft, lastPos.y - this.offsetTop);
+	setPosition(e);
+	context?.lineTo(lastPos.x - this.offsetLeft, lastPos.y - this.offsetTop)
+	context && (context.strokeStyle = color);
+	context?.stroke();
+})
+
+document.getElementById("line")?.addEventListener('click', function(e) {
+	console.log('draw line');
+})
+
+document.getElementById("square")?.addEventListener('click', function(e) {
+	console.log('draw square');
+})
+
+document.getElementById("circle")?.addEventListener('click', function(e) {
+	console.log('draw circle');
+})
+
+document.getElementById("pen")?.addEventListener('click', function(e) {
+	console.log('draw pen');
+})
+
+document.getElementById("eraser")?.addEventListener('click', function(e) {
+	console.log('draw eraser');
+})
+
+document.getElementById("download")?.addEventListener('click', function(e) {
+	console.log('draw download');
+})
+
+document.getElementById("upload")?.addEventListener('click', function(e) {
+	console.log('draw upload');
+})
+
+document.getElementById("clear")?.addEventListener('click', function(e) {
+	context?.clearRect(0, 0, context.canvas.width, context.canvas.height);
+	drawingHistory.splice(0, drawingHistory.length)
+	historyPointer = -1;
+	saveCanvasState();
+})
+
+document.getElementById("undo")?.addEventListener('click', undo)
+
+document.getElementById("redo")?.addEventListener('click', redo)
+
+function saveCanvasState() {
+	historyPointer++;
+	const canvasState = context?.canvas.toDataURL();
+	console.log(drawingHistory)
+	drawingHistory.splice(historyPointer);
+	drawingHistory.push(canvasState);
+	if (historyPointer > maxHistoryLength) {
+		drawingHistory.shift();
+		historyPointer--;
+	}
+	console.log('end save ', drawingHistory);
+}
+
+function undo() {
+	console.log('undo:', drawingHistory.length)
+	if (historyPointer > 0) {
+		const previousState = new Image();
+		previousState.src = /** @type string */ (drawingHistory[historyPointer - 1]);
+		previousState.onload = function () {
+			context?.clearRect(0, 0, context.canvas.width, context.canvas.height); 
+			context?.drawImage(previousState, 0, 0);
+		};
+		historyPointer--; // Remove the last canvas state
+	}
+	console.log('after undo:', drawingHistory.length)
+}
+
+function redo() {
+	console.log('redo:', drawingHistory.length)
+	if (historyPointer + 1 < drawingHistory.length) {
+		const previousState = new Image();
+		previousState.src = /** @type string */ (drawingHistory[historyPointer + 1]);
+		previousState.onload = function () {
+			context?.clearRect(0, 0, context?.canvas.width, context?.canvas.height);
+			context?.drawImage(previousState, 0, 0);
+		};
+		historyPointer++;
+	}
+	console.log('after redo:', drawingHistory.length)
+}
+
+// $('#painter').mousedown(function(e){
+// 	var mouseX = e.pageX - this.offsetLeft;
+// 	var mouseY = e.pageY - this.offsetTop;
+		  
+// 	paint = true;
+// 	addDot(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+// 	redraw();
+//   });
+  
+//   $('#painter').mousemove(function(e){
+// 	if(paint){
+// 	  addDot(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+// 	  redraw();
+// 	}
+//   });
+  
+//   // Mouse Up Event: Marker is off the paper; paint boolean will remember!
+//   $('#painter').mouseup(function(e){
+// 	paint = false;
+//   });
+  
+//   // Mouse Leave Event: If the marker goes off the paper
+//   $('#painter').mouseleave(function(e){
+// 	paint = false;
+//   });
+  
+//   //	user picks a color
+//   $('.picker').on('click', function(e){
+// 	  curColor = $(this).css('background-color');
+//   });
+  
+//   //	user clears data
+//   $('#btnClear').on('click', function(e){
+// 	clickX = new Array();
+// 	  clickY = new Array();
+// 	  clickDrag = new Array();
+// 	  clickColor = new Array();
+// 	  clickSize = new Array();
+// 	// Clears the canvas
+// 	context.clearRect(0, 0, context.canvas.width, context.canvas.height); 
+//   })
+  
+//   //  save data
+//   $('#btnSave').on('click', function(e){
+// 	var canvas = document.getElementById('painter');
+// 	var data = canvas.toDataURL();
+// 	window.open(data);
+//   });
